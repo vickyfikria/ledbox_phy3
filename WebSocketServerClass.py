c74c6c5173ba141772b9af57da3ef7822d17f8b7
vickyfikria <vicky.fikria@gmail.com>
2024-03-26 03:46:36 +0700

    Update WebSocketServerClass.py for first image

--- WebSocketServerClass.py
+++ WebSocketServerClass.py
@@ -1,11 +1,5 @@
-# uncompyle6 version 3.7.4
-# Python bytecode 2.7 (62211)
-# Decompiled from: Python 2.7.16 (default, Oct 10 2019, 22:02:15) 
-# [GCC 8.3.0]
-# Embedded file name: /home/pi/ledbox/WebSocketServerClass.py
-# Compiled at: 2021-02-18 07:58:54
 import ledboxApp as app
-from websockets import WebsocketServer
+from websocket_server import WebsocketServer
 import json
 
 class WebSocketServerClass:
@@ -13,8 +7,8 @@ class WebSocketServerClass:
     connections = []
     name = 'WebSocket'
 
-    def __init__(self, port):
-        self.server = WebsocketServer(port, host='0.0.0.0')
+    def __init__(self):
+        self.server = WebsocketServer(port=50007, host='0.0.0.0')
         self.server.set_fn_new_client(self.new_client)
         self.server.set_fn_message_received(self.onMessage)
         self.server.set_fn_client_left(self.client_left)
@@ -31,16 +25,20 @@ class WebSocketServerClass:
         self.connections.append(client)
         app.addClient(self, client)
         server.send_message(client, json_data)
+        print('New Client'+str(client['id'])+ ' comes')
 
     def client_left(self, client, server):
         app.removeClient(self, client)
         if client != None:
             self.connections.remove(client)
+        print(str(client['id'])+ ' Client left')
         return
 
     def onMessage(self, client, server, message):
         c = app.getClientBySocketClient(self, client)
         response = app.processMessage(message, c)
+        print('[WS] Response from client ' + str(client['id']))
+        print(response)
         server.send_message(client, response)
 
     def send(self, message):
