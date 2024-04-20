7a71ca5f045885a800a1842b3cca978ea0f9c3ae
vickyfikria <vicky.fikria@gmail.com>
2024-04-20 13:54:01 +0700

    Update testProcedure.py

--- testProcedure.py
+++ testProcedure.py
@@ -1,10 +1,7 @@
-# uncompyle6 version 3.7.4
-# Python bytecode 2.7 (62211)
-# Decompiled from: Python 2.7.16 (default, Oct 10 2019, 22:02:15) 
-# [GCC 8.3.0]
-# Embedded file name: /home/pi/ledbox/testProcedure.py
-# Compiled at: 2021-02-18 07:58:54
-import ledboxApp as app, time, threading, gzip, io, json, base64, urllib
+import ledboxApp as app, time, threading, gzip, json, base64
+from io import BytesIO
+from io import StringIO
+from urllib.request import urlopen
 check_usb = 0
 check_display = 0
 status = 1
@@ -15,11 +12,13 @@ thread_check = None
 base = 'http://ledbox.tech4sport.com/'
 
 def run(onlineVerify=False):
-    app.isTestMode = True
-    app.Debug('RUN TEST LEDbox')
+    #app.isTestMode = True
+    #app.Debug('RUN TEST LEDbox')
+    print('#1')
     testDisplay()
+    print('#2')
     testSerialCable()
-    app.Debug(str(onlineVerify))
+    print('#3')
     if onlineVerify == True:
         thread_check = threading.Thread(target=checkTest)
         thread_check.start()
@@ -29,11 +28,21 @@ def checkDeviceVerification():
     global base
     try:
         url = base + 'api.php?task=getVerification&serialnumber=' + app.deviceName
-        response = urllib.urlopen(url, timeout=2).read()
-        device = json.loads(response)
-        if int(device['device']['verified']) == 0:
+        with urlopen(url) as response :
+            body = response.read()
+        # response = urlopen(url, timeout=2).read()
+        print(bytes(body).decode())
+        son = json.loads(body)
+        print(son['status'])
+        print(son['device']['verified'])
+        # son['device']['verified'] = 0 means not yet verified by admin
+        # son['device']['verified'] = 1 means already confirm by admin
+        # through https://ledbox.tech4sport.com/index.php?view=listDevices  admin page 
+        if son['device']['verified'] == 0:
+            print(f' {app.deviceName} is verified')
             run(True)
-    except:
+    except Exception as e:
+        print(f' {app.deviceName} Device verification fail : {e}')
         pass
 
 
@@ -50,12 +59,22 @@ def checkTest():
             thread_display.join()
             thread_send.join()
             app.current_layout = app.layoutManager.loadLayout('waiting')
-            r = 'Display OK\nUSB OK'
-            result = base64.b64encode(bytes(r))
+            r = 'Display OK\nUSB OK'.encode() #this str need to encode to bytes 
+            print(r)
+
+            result = base64.b64encode(r)
+            print('result : ')
+            print(result)
+
             verify = True
-            url = base + 'api.php?task=setVerification&serialnumber=' + app.deviceName + '&verify=' + str(verify) + '&result=' + result
-            response = urllib.urlopen(url).read()
-            app.Debug('Verification Send to ' + url)
+            url = base + 'api.php?task=setVerification&serialnumber=' + app.deviceName + '&verify=' + str(verify) + '&result=' + result.decode()
+            print(url)
+            '''with urlopen(url) as response:
+                body = response.read()
+            print(bytes(body).decode())
+            #response = urllib.urlopen(url).read()
+            app.Debug('Verification Send to ' + url)'''
+            checkUSBSendend(0)
             break
 
 
@@ -75,6 +94,7 @@ def changeColorDisplay():
     global check_display
     while True:
         if stopThread == True:
+            print('Stop changeColorDisplay thread')
             break
         app.current_layout = app.layoutManager.loadLayout('test')
         app.current_layout.setSection('rectangle', 'color', '255,0,0')
@@ -92,21 +112,30 @@ def sendMessageFromUSB():
     global status
     while True:
         if stopThread == True:
+            print('Stop sendMessageFromUSB thread')
             break
         if status == 1:
             data = {}
             data['cmd'] = 'CheckTest'
             data['value'] = 'test'
             json_data = json.dumps(data)
+            print(json_data)
             app.Debug('SEND from USB: ' + json_data)
             time.sleep(2)
-            out = StringIO()
+            out = BytesIO() #use BytesIO for python3 instead StringIO
             with gzip.GzipFile(fileobj=out, mode='wb') as (gz):
-                gz.write(json_data)
+                gz.write(json_data.encode('utf-8'))
                 gz.close()
-                coding_message = out.getvalue()
+                coding_message = out.getvalue() # take value from Buffer out as string
+                # problem gzip signature will not 
+		# coding_message = out.getvalue() + '/r/n'.encode(), ERROR initial_value must be str
+		# coding_message =  b'\x1f\x8b\x08\....\x00\x00\x00/r/n'
+            print(f' coding message : {out.getvalue()}')
+            print(f' out.read : {out.read()}')
+
+            print(f' out : {out}')
             try:
-                app.serialUSBClient.write(coding_message + '\r\n')
+                app.serialUSBClient.write(out) 
             except Exception as e:
                 app.Debug('ERROR SEND USB ' + str(e))
                 print ('ERROR - cable disconnected ' + str(e))
@@ -125,3 +154,4 @@ def checkUSBSendend(value):
     data['value'] = data_value
     app.API.Horn(data)
     check_usb = check_usb + 1
+    print('checkUSBSendend done')
